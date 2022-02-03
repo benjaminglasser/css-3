72ecc74fd244d0bfdedaee264d3fd53c0053f809
ben glasser <ben@fantasticvoyage.com>
2022-02-02 21:01:18 -0800

    takes out api call

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -4,10 +4,11 @@ function Article(props) {
     console.log(props)
     return (
         <div className="flex flex-col items-center w-full h-max p-5 border-solid border-black border-2 mb-4">
-            <h1 className="text-xl mb-4">{props.info.title}</h1>
-            <h3 className="text-xs mb-4">{props.info.content}</h3>
-            <img src={props.info.urlToImage} alt="image from article"></img>
-            <a className="mt-4 px-2 py-1 hover:bg-black hover:text-white rounded-full" href={props.info.url} target="_blank">Read More</a>
+            <h1 className="text-xl mb-4">This is a title</h1>
+            <h3 className="text-xs mb-4">lorem
+                This is a description Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, delectus non? Fuga illum animi veniam tempore repellat quidem. Maxime, voluptatem mollitia consectetur odio quibusdam beatae doloremque obcaecati quod provident cupiditate.</h3>
+            {/* <img src={props.info.urlToImage} alt="image from article"></img> */}
+            <a className="mt-4 px-2 py-1 hover:bg-black hover:text-white rounded-full" href="" target="_blank">Read More</a>
         </div >
     )
 }
--- src/components/Right/Right.js
+++ src/components/Right/Right.js
@@ -1,44 +1,61 @@
 import React from 'react';
-import Axios from 'axios'
-import { useEffect, useState } from 'react'
+// import Axios from 'axios'
+// import { useEffect, useState } from 'react'
 import Article from "../Article/Article"
 
 function Right() {
 
-    const [articles, setArticles] = useState([])
-    const [isLoading, setLoading] = useState(true);
+    // const [articles, setArticles] = useState([])
+    // const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getItem()
+    // useEffect(() => {
+    //     setArticles([])
 
-    }, [])
+    // }, [])
+
+    // let articles = [
+    //     ["This is a title"]
+    // ]
+
+
+    // const getItem = () => {
+    //     Axios.get('https://newsapi.org/v2/everything?q=Apple&from=2022-02-03&sortBy=popularity&apiKey=a069e917715e4dffbbae4cfe81b8a316').then((res) => {
+    //         setArticles([res])
+    //         setLoading(false);
+
+    //     })
+    // }
 
 
 
-    const getItem = () => {
-        Axios.get('https://newsapi.org/v2/everything?q=Apple&from=2022-02-03&sortBy=popularity&apiKey=a069e917715e4dffbbae4cfe81b8a316').then((res) => {
-            setArticles([res])
-            setLoading(false);
+    // if (isLoading) {
+    //     return <div className="App">Loading...</div>;
+    // }
 
-        })
-    }
-
-    if (isLoading) {
-        return <div className="App">Loading...</div>;
-    }
 
     return (
         <>
             <div className="p-4 flex flex-col border-solid border-4 border-t-0 border-r-0 border-black justify-between h-max "
             >
-                {
-                    articles[0].data.articles.map((a, idx) =>
-                        <div>
-                            <Article key={idx} info={a} />
-                        </div>
+                {/* {
+                    articles[0].data.articles.map((a, idx) => */}
+                {/* <div> */}
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                <Article />
+                {/* </div>
                     )
 
-                }
+                } */}
             </div>
         </>
     )
